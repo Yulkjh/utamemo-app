d60965de42efb0c291bea17217242e92f2d619f9
Yu minato <yumianto756@gmail.com>
2026-02-23 20:46:49 +0900

    Admin: show username, superuser status, and filters in user list

--- myproject/users/admin.py
+++ myproject/users/admin.py
@@ -53,22 +53,21 @@ secure_admin_site = SecureAdminSite(name='secure_admin')
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    # ユーザー名とパスワードを非表示にしたカスタムfieldsets
     fieldsets = (
-        # ('認証情報', {'fields': ('username', 'password')}),  # 非表示
+        ('認証情報', {'fields': ('username',)}),
         ('個人情報', {'fields': ('first_name', 'last_name', 'email')}),
         ('権限', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('日時', {'fields': ('last_login', 'date_joined')}),
         ('追加情報', {'fields': ('bio', 'profile_image', 'plan', 'stripe_customer_id')}),
     )
     
-    # 一覧表示でもユーザー名を匿名化
-    list_display = ('id', 'email', 'plan', 'is_staff', 'date_joined')
-    list_display_links = ('id',)  # IDでリンク
+    # 一覧表示：ユーザー名・権限・プランを表示
+    list_display = ('id', 'username', 'email', 'plan', 'is_superuser', 'is_staff', 'is_active', 'date_joined')
+    list_display_links = ('id', 'username')
+    list_filter = ('is_superuser', 'is_staff', 'is_active', 'plan')
     ordering = ('-date_joined',)
     
-    # ユーザー名での検索を無効化
-    search_fields = ('email', 'first_name', 'last_name')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
     
     # 読み取り専用フィールド
     readonly_fields = ('last_login', 'date_joined')
