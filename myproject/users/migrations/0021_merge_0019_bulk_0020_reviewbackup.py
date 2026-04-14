eef286898c55301740c2966467763577e16456e6
Yu minato <yumianto756@gmail.com>
2026-04-15 08:30:34 +0900

    fix: merge conflicting migrations 0019_bulk + 0020_reviewbackup

---
+++ myproject/users/migrations/0021_merge_0019_bulk_0020_reviewbackup.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0019_bulk_create_reviews'),
+        ('users', '0020_reviewbackup_trainingdatareview_deleted_at_and_more'),
+    ]
+
+    operations = [
+    ]
